67cf14788d950f938ac301c4424d0ab2fb9834b6
Femi <femi96@mit.edu>
2018-03-07 15:05:48 -0500

    Added place changes

--- Assets/Scripts/Places/Pizza.cs
+++ Assets/Scripts/Places/Pizza.cs
@@ -3,27 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PizzaPlace : Place {
-	// Place:
+	// PizzaPlace:
 	//		Data type that holds a position's data.
 
+	public PizzaPlace() {}
 
-	// Place variables
-	private bool owned;
-	private Wand owner;	// If owner == null, owner = false
-
-
-	// Called when trying to takeover a location.
-	//		Takes wand of attempting player, for player's money and manpower
-	//		Returns boolean for if takeover was successful
-	//
-	public virtual bool TakeOver(Wand player) {
-		return false;
+	public override bool TakeOver(Wand player) {
+		return true;
 	}
 
-	// Called on generation ticks.
-	//		Change resources of owner based on place effect
-	//
-	public virtual void Generate() {
+	public override void Generate() {
+		Wand owner = GetOwner();
 		return;
 	}
 }
