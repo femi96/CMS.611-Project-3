b824fb855ed89273c8ad501aa81adc9ec48c536b
Femi <femi96@mit.edu>
2018-03-11 18:19:56 -0400

    Added UI

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour {
 	// Game:
 	//		Controller that handles game data.
 
-
+	
 	// Game variables
 	private Wand wand1;
 	private Wand wand2;
 	private Map map;
+	
+	// Game variables
+	public GameObject wandUI1;
+	public GameObject wandUI2;
 
 	// Game tick variables
 	private float time;
@@ -46,5 +51,15 @@ public class Game : MonoBehaviour {
 				map.GetPlace(x, y).Generate();
 			}
 		}
+		UpdateCanvasUI();
+	}
+
+	// Update cavnasUI each tick
+	void UpdateCanvasUI() {
+		wandUI1.transform.Find("MValue").gameObject.GetComponent<Text>().text = wand1.GetMoney().ToString();
+		wandUI1.transform.Find("PValue").gameObject.GetComponent<Text>().text = wand1.GetManPower().ToString();
+
+		wandUI2.transform.Find("MValue").gameObject.GetComponent<Text>().text = wand2.GetMoney().ToString();
+		wandUI2.transform.Find("PValue").gameObject.GetComponent<Text>().text = wand2.GetManPower().ToString();
 	}
 }
